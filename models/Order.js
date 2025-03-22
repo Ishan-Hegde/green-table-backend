b38f0ecd31dc0134bf7f2282eac73fb5d4d328f4
Ishan-Hegde <110191499+Ishan-Hegde@users.noreply.github.com>
2025-03-22 23:39:33 +0530

    Revert "new tests"
    This reverts commit c2838dd350bfdd42a97d2abf828a80eaddc0eaac.

--- models/Order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const orderSchema = new mongoose.Schema({
-    item: String,
-    quantity: Number,
-    status: String
-});
-
-module.exports = mongoose.model('Order', orderSchema);
