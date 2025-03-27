9a78ecf1b3d7387fab2fb4ee1cfdd6634ce050cd
Ishan-Hegde <110191499+Ishan-Hegde@users.noreply.github.com>
2025-03-28 00:12:30 +0530

    changed to URI

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes');
@@ -11,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/greentable')
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
